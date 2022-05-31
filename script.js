095dd5d67099c3fa5d2128fd6171e2b3d667bc66
sofiatenorius <sofiaestudante29@gmail.com>
2022-05-31 17:34:02 -0300

    First commit
    This is a small form created to train DOM modulation, create functions, arrays and objects. The intention with this project is to apply the concepts studied

---
+++ script.js
@@ -0,0 +1,37 @@
+function meuEscopo(){
+    const form = document.querySelector(".form");
+    const resultado = document.querySelector (".result")
+
+    const person= [];
+    //----------- Outra forma mais complicada de fazer ----------
+    
+    // form.onsubmit= function(evento){
+    //     evento.preventDefault();
+    //     alert(1)
+    //     console.log ("Foi enviado.")
+    // };
+
+    let contador =1
+    function recebeEventoForm (evento){
+        evento.preventDefault();
+
+    const nome = form.querySelector ('.nome'); 
+    const sobrenome= form.querySelector ('.sobrenome'); 
+    const peso= form.querySelector ('.peso'); 
+    const altura = form.querySelector ('.nome'); 
+
+    person.push({
+        nome:nome.value,
+        sobrenome: sobrenome.value,
+        peso:peso.value,
+        altura:altura.value
+    });
+    console.log (person);
+
+    resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} +
+     ${peso.value} ${altura.value}</p>`;
+}
+
+    form.addEventListener('submit', recebeEventoForm)
+}
+meuEscopo ()
